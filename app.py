12f13a5fa702643a7167b360880d565609980098
bnspriyatham <49431466+bnspriyatham@users.noreply.github.com>
2021-06-30 19:57:52 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, jsonify, request
+from werkzeug.utils import secure_filename
+from pixellib.instance import custom_segmentation
+import base64
+from io import BytesIO
+from PIL import Image
+
+
+INPUT_IMAGE = ".input.jpg"
+OUTPUT_IMAGE = ".output.jpg"
+
+#app
+app = Flask(__name__)
+
+#routes
+@app.route('/',methods = ['POST'])
+
+def post():
+    image = request.get_json()
+    image_string = base64.b64decode(image['file'])
+    image_data = BytesIO(image_string)
+    img = Image.open(image_data)
+    img.save(INPUT_IMAGE)
+    segment_image = custom_segmentation()
+    segment_image.inferConfig(num_classes= 1, class_names= "scratch")
+    segment_image.load_model("mask_rcnn_model.020-1.088950.h5")
+    instance_segment_image.segmentImage(INPUT_IMAGE, output_image_name=OUTPUT_IMAGE, show_bboxes=True)
+    print("output received")
+    with open(OUTPUT_IMAGE, "rb") as img_file:
+        my_string = base64.b64encode(img_file.read())
+        final_base64_image_string = my_string.decode('utf-8')
+    return print(final_base64_image_string)
+    
+if __name__ == '__main__':
+    app.run(port = 5000, debug=True)
